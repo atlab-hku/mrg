ee9c54dc0e2ee7819ce57c8a8af02941664e17fe
gabe <ggetzie@gmail.com>
2021-06-20 10:36:28 +0800

    remove data from repo

--- collect.py
+++ collect.py
@@ -13,13 +13,14 @@ base_url = "https://www.loc.gov/pictures/search/"
 base_params = {
     "q": "mrg",
     "sp": 1
-    }
+}
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
 }
 
 DETAIL_FOLDER = pathlib.Path("download/detail")
+
 
 def get_or_exit(url, params=None, err_msg="error retrieving page"):
 
@@ -36,19 +37,19 @@ def get_or_exit(url, params=None, err_msg="error retrieving page"):
 def get_results():
     last_list = int(open("download/last_list.txt").read().strip())
 
+    visited_details = set(
+        [d.name for d in DETAIL_FOLDER.iterdir() if d.is_dir()])
 
-    visited_details = set([d.name for d in DETAIL_FOLDER.iterdir() if d.is_dir()])
-    
     for i in range(last_list, 587):
         params = {
             "q": "mrg",
             "sp": i
         }
-        
+
         r = get_or_exit(base_url, params, "Error retrieving list page.")
         print(f"Got list page #{i} - {r.url}")
         soup = BeautifulSoup(r.content, features="lxml")
-    
+
         with open(f"download/list/{i}.html", "w") as list_page:
             list_page.write(soup.prettify())
         # get list of links to detail pages
@@ -59,7 +60,8 @@ def get_results():
             if id_num in visited_details:
                 print(f"Already visited {id_num} - skipping")
                 continue
-            dr = get_or_exit(link["href"], err_msg="Error retrieving detail page")
+            dr = get_or_exit(
+                link["href"], err_msg="Error retrieving detail page")
             print(f"Got detail page for {id_num} - {dr.url}")
             ds = BeautifulSoup(dr.content, features="lxml")
 
@@ -80,7 +82,8 @@ def get_results():
             # save JPG
             jpeg_links = ds.find_all("a", string=re.compile(r'JPEG \(\d+kb\)'))
             jpeg_href = jpeg_links[-1]["href"]
-            jpeg_url = f"https:{jpeg_href}" if jpeg_href.startswith("/") else jpeg_href
+            jpeg_url = f"https:{jpeg_href}" if jpeg_href.startswith(
+                "/") else jpeg_href
             time.sleep(1)
             jr = requests.get(jpeg_url)
             print(f"Got JPEG - {jr.url}")
@@ -95,9 +98,9 @@ def get_results():
                 detail_count.write(f"{i} - {id_num}")
             time.sleep(random.randint(3, 10))
 
-        
         with open(f"download/last_list.txt", "w") as list_count:
             list_count.write(f"{i}")
+
 
 def get_jpeg_url(soup):
     jpeg_links = soup.find_all("a", string=re.compile(r'JPEG \(\d+kb\)'))
@@ -115,6 +118,7 @@ def missing_jpgs():
                 res.append(d.name)
     return res
 
+
 def get_jpgs():
     missing = missing_jpgs()
     for folder in missing:
@@ -122,10 +126,11 @@ def get_jpgs():
         soup = BeautifulSoup(detail.open().read())
         url = get_jpeg_url(soup)
         r = get_or_exit(url)
-        img_file =  DETAIL_FOLDER / folder / f"{folder}.jpg"
+        img_file = DETAIL_FOLDER / folder / f"{folder}.jpg"
         img_file.write_bytes(r.content)
         print(f"Wrote {img_file.absolute()}")
         time.sleep(3)
+
 
 def rename_jpgs(dry_run=False):
     for d in DETAIL_FOLDER.iterdir():
@@ -134,7 +139,36 @@ def rename_jpgs(dry_run=False):
         target = d / f"{id_num}.jpg"
         if dry_run:
             print(f"Rename {jpg.absolute()} to {target.absolute()}")
-            c =  input("Continue? [Y/N]: ").upper()
-            if c != "Y": break
+            c = input("Continue? [Y/N]: ").upper()
+            if c != "Y":
+                break
         else:
             jpg.rename(target)
+
+# Sample record to generate - index by id number
+# rec = {
+#     2017702119: {
+#         "title": "Premium gas station (formerly Joy gas station), Lakeshore Boulevard & Windemere, Toronto, Ontario, Canada",
+#         "year": 1977,  # 260 - - c
+#         # 524 8 - a
+#         "credit": "John Margolies Roadside America photograph archive (1972-2008), Library of Congress, Prints and Photographs Division.",
+#         "country": "Canada",  # 752 - - a
+#         "state": "Ontario",  # 752 - - b
+#         "city": "Toronto",  # 752 - - c
+#         "artist": "Margolies, John",  # 773 0 - a
+#         "latlong": [43.70011,-79.4163] # get from innogeo
+#     }
+# }
+
+def table_to_dict(soup):
+    # extract table from MARC record html and convert to dict
+    # headers: Tag, i1, i2, code, text
+    # format = {
+    #     "001": { 
+    #         "i1": "0",
+    #         "i2": "0",
+    #         "code": "a",
+    #         "text": "example"
+    #     }
+    # }
+    pass
